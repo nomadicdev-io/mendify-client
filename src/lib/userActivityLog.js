bd3a8a554ea527ada952e80d9ad63303130f3b54
nomadic-dev <nomadicdev.io@gmail.com>
2025-09-03 05:03:20 +0400

    new updates

--- src/lib/userActivityLog.js
+++ src/lib/userActivityLog.js
@@ -1,10 +1,31 @@
 import { PB } from "../App";
 import getBrowserName from "./getBrowserName";
 import getIP from "./getIP";
-import getOSName from "./getOsName";
 import getGeoLocation from "./getGeoLocation";
 
 async function userActivityLog(type, user) {
+
+    const userAgent = window.navigator.userAgent || window.navigator.vendor || window.opera;
+    let os
+
+    if (/windows phone/i.test(userAgent)) {
+      os = "Windows Phone";
+    }
+    if (/win/i.test(userAgent)) {
+      os = "Windows";
+    }
+    if (/android/i.test(userAgent)) {
+      os = "Android";
+    }
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      os = "iOS";
+    }
+    if (/Macintosh|MacIntel|MacPPC|Mac68K/.test(userAgent)) {
+      os = "Mac OS";
+    }
+    if (/linux/i.test(userAgent)) {
+      os = "Linux";
+    }
 
     const { record } = PB.authStore
 
@@ -13,7 +34,7 @@ async function userActivityLog(type, user) {
         type: type || 'unknown',
         date: new Date(),
         browser: getBrowserName(),
-        os: getOSName(),
+        os: os,
         ip: await getIP(),
         location: await getGeoLocation(),
     }
